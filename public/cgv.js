b1eb05997196da89c2c6023f1c92ab3e524d21bb
Hyun <hwn123h@naver.com>
2023-08-14 11:55:48 +0900

    doing crawling cgv time

--- public/cgv.js
+++ public/cgv.js
@@ -1,4 +1,30 @@
-function getSelectedValue () {
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function getMovieTimes() {
+    try {
+        const response = await axios.get('http://www.cgv.co.kr/theaters/?areacode=02&theaterCode=0298&date=20230814');
+        const html = response.data;
+
+        const $ = cheerio.load(html);
+        const movieTimes = [];
+
+        $('.col-times').each((index, element) => {
+            const movieTime = $(element).text().trim();
+            movieTimes.push(movieTime);
+        });
+
+        console.log('영화 시간:', movieTimes);
+    } catch (err) {
+        console.err('에러 발생:', err);
+    }
+}
+
+
+
+
+
+function getSelectedValue() {
     //오픈 마감 마감2
     const radioGroup = document.getElementsByName('part_time');
     let Part;
@@ -11,7 +37,7 @@ function getSelectedValue () {
 
     //영화시간
     Time = document.getElementById('time').value;
-
+    
     //근무시간
     const radioGroup2 = document.getElementsByName('work_time');
     let Work;
